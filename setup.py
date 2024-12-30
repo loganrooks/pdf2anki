dc073f288d451c7d2274f0e78b39578d13d4d917
loganrooks <logansrooks@gmail.com>
2024-12-30 17:50:43 -0500

    altered setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="pdf2anki",
-    version="0.1.0",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
         "pdfminer.six",
         "pytest",
         "fitz",
         "dataclasses; python_version<'3.7'", 
-        # ...other dependencies...
+
     ],
     entry_points={
         "console_scripts": [
-            # Example: "pdf2anki=pdf2anki.cli:main"
+            "pdf2anki = pdf2anki.__main__:main"
         ]
     },
 )
