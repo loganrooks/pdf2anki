0d8499cda458c96555dd994962cd24bc62102fff
loganrooks <logansrooks@gmail.com>
2025-01-02 16:22:26 -0500

    added progress monitor decorator

--- pdf2anki/decorators.py
+++ pdf2anki/decorators.py
@@ -1,5 +1,8 @@
+from functools import wraps
 import logging
 import time
+
+import tqdm
 
 def log_time(func):
     """Decorator to log the time a function takes to run."""
@@ -9,4 +12,17 @@ def log_time(func):
         end_time = time.time()
         logging.info(f"Function {func.__name__} took {end_time - start_time:.4f} seconds")
         return result
+    return wrapper
+
+def progress_monitor(func):
+    """Decorator to display progress for a function with a verbose flag."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        verbose = kwargs.get('verbose', False)
+        if verbose:
+            # Assuming the function returns an iterable
+            iterable = func(*args, **kwargs)
+            return list(tqdm(iterable, desc=func.__name__))
+        else:
+            return func(*args, **kwargs)
     return wrapper
