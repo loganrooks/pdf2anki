1b2e080c15d9b6ab9c1feae36f2fdda91f478676
loganrooks <logansrooks@gmail.com>
2025-01-05 18:00:51 -0500

    modified the extract_headers, extract_text_for_headers, and merge_toc_entries to take into account new ToCEntry structure as well as the new functions for merging and cleaning incomplete lines and paragraphs,

--- pdf2anki/recipe.py
+++ pdf2anki/recipe.py
@@ -5,6 +5,7 @@ import json
 import logging
 import os
 import pickle
+from pdf2anki.extraction import merge_split_paragraphs
 from pdf2anki.utils import clean_text, contained_in_bbox, get_text_index_from_vpos
 from pdf2anki.decorators import log_time
 from typing import Callable, List, Literal, Optional, Set, Tuple, Pattern, Union, overload
@@ -258,11 +259,12 @@ class Recipe:
                 if toc_filter.admits(paragraph):
                     entry = ToCEntry(
                         level=toc_filter.vars.level,
-                        title=paragraph.text.strip(),
-                        page=pagenum,
-                        vpos=paragraph.bbox[3],  # top y-value as vertical position
+                        title=paragraph.text.strip() if not toc_filter.opts.greedy else paragraph.text.strip(),
+                        pagenum=pagenum,
+                        page_range=[pagenum, pagenum],
+                        start_vpos=paragraph.bbox[1],  # bottom y-value as vertical position
                         bbox=paragraph.bbox,
-                        text=paragraph.text,
+                        text="",
                         subsections=[]
                     )
                     # Associate matched TextFilters for later use
@@ -271,31 +273,36 @@ class Recipe:
                     break  # if multiple filters match, we can stop at the first
         return toc_entries
 
-    def extract_text_for_headers(self, doc: List[PageInfo], toc_entries: List[ToCEntry]) -> None:
+    def extract_text_for_headers(self, doc: List[PageInfo], toc_entries: List[ToCEntry], page_range: Optional[Tuple[int, int]] = None) -> 'Recipe':
+        # TODO: Change the logic for this such that the end_page is actually the last page of the current entry and not the first page of the next entry
+        # This would require some check to see if the end_vpos as it currently stands is near the top of the page and to use the end_vpos of the final paragraph
+        # from the previous page
+        # Will also need to change the logic of nest_toc_entries
+        if page_range:
+            assert len(page_range) == 2 and page_range[0] <= page_range[1]
+        else:
+            page_range = (1, len(doc))
+            
         for i, entry in enumerate(toc_entries):
-            merged_text = []
-            end_page = toc_entries[i+1].pagenum if i+1 < len(toc_entries) else None
-            end_vpos = toc_entries[i+1].bbox[3] if i+1 < len(toc_entries) else None
-            for page in doc:
-                if page.pagenum < entry.pagenum:
-                    continue
-                if end_page is not None and page.pagenum >= end_page:
-                    break
-
-                for paragraph in page.paragraphs:
-                    if page.pagenum == entry.pagenum and paragraph.bbox[3] > entry.start_vpos:
-                        # skip paragraphs above the header
-                        continue
-                    if end_page is not None and page.pagenum == end_page - 1 and paragraph.bbox[3] <= end_vpos:
-                        # stop at next header's vpos
-                        break
+            filtered_paragraphs = []
+            end_page = toc_entries[i+1].pagenum if i+1 < len(toc_entries) else page_range[1]
+            entry.page_range[1] = end_page
+            end_vpos = toc_entries[i+1].bbox[3] if i+1 < len(toc_entries) else 0
+            entry.end_vpos = end_vpos
+            
+            paragraphs = [paragraph for paragraph in doc[entry.page_range[0]].paragraphs if paragraph.bbox[3] <= entry.start_vpos]\
+                         + [paragraph for page in doc[entry.page_range[0]+1:entry.page_range[1]-1] for paragraph in page.paragraphs]\
+                         + [paragraph for paragraph in doc[entry.page_range[1]-1].paragraphs if paragraph.bbox[3] > end_vpos]
+            merged_paragraphs = merge_split_paragraphs(paragraphs)
+            for paragraph in merged_paragraphs:
                     for text_filter_id in entry.text_filter_ids:
                         text_filter = self.text_filters[text_filter_id]
                         if text_filter.admits(paragraph):
-                            merged_text.append(paragraph.text)
+                            filtered_paragraphs.append(paragraph.text)
                             break
 
-            entry.text = entry.title + "\n\n" + "\n".join(merged_text)
+            entry.text = "\n\n".join(filtered_paragraphs)
+        return self
 
     def extract_toc(self, doc: List[PageInfo], page_range: Optional[Tuple[int, int]] = None, extract_text: bool = False) -> List[ToCEntry]:
         all_toc_entries = []
@@ -309,9 +316,9 @@ class Recipe:
         for page in doc[start_page-1:end_page]:
             page_entries = self.extract_headers(page.paragraphs, page.pagenum)
             all_toc_entries.extend(page_entries)
-
+        merged_all_toc_entries = merge_toc_entries(all_toc_entries, tolerance=30)
         if extract_text:
-            self.extract_text_for_headers(doc, all_toc_entries)
+            self.extract_text_for_headers(doc, merged_all_toc_entries, page_range)
 
         return all_toc_entries
 
@@ -513,18 +520,18 @@ def generate_recipe(doc: List[PageInfo],
         header_page_num, header_str = header_text
 
         # Extract metadata for the header
-        page_index = page_numbers.index(header_page_num - 1) if page_numbers else header_page_num - 1
+        # page_index = page_numbers.index(header_page_num - 1) if page_numbers else header_page_num - 1
         # page = doc[page_index]
         header_elements = [header_element for header_element in \
                             extract_elements(doc, header_str, [header_page_num], ign_case=ign_case, \
-                                                ign_pattern=ign_pattern, tolerance=tolerances["bbox"], clip=clip, element_type=ElementType.LINE)\
-                                if isinstance(header_element, LineInfo)]
+                                                ign_pattern=ign_pattern, tolerance=tolerances["bbox"], clip=clip, element_type=ElementType.PARAGRAPH)\
+                                if isinstance(header_element, ParagraphInfo)]
 
         if len(header_elements) > 1:
             header_elements.sort(key=lambda x: x.char_width)
-        line = header_elements[-1] if len(header_elements) > 0 else None
+        paragraph = header_elements[-1] if len(header_elements) > 0 else None
 
-        if line is None:
+        if paragraph is None:
             logging.warning(f"No header found for {header_str} on page {header_page_num}.")
             continue
         
@@ -534,13 +541,13 @@ def generate_recipe(doc: List[PageInfo],
             "bbox": toc_filter_options[i].get("bbox", BoundingBoxFilterOptions())
         }
 
-        toc_filter = ToCFilter.from_line_info(line, header_level, fltr_dict)
+        toc_filter = ToCFilter.from_paragraph_info(paragraph, header_level, fltr_dict)
         header_filters.append(toc_filter)
 
         if include_text_filters:
             text_filters: List[TextFilter] = []
             for (text_page_num, text_str) in header["text"]:
-                page_index = page_numbers.index(text_page_num - 1) if page_numbers else text_page_num - 1
+                # page_index = page_numbers.index(text_page_num - 1) if page_numbers else text_page_num - 1
                 # page = doc[page_index] 
 
                 text_elements = set([text_element for text_element in \
@@ -567,16 +574,16 @@ def generate_recipe(doc: List[PageInfo],
     return Recipe(recipe_dict)
 
 @log_time
-def merge_toc_entries(toc_entries, tolerance=10):
+def merge_toc_entries(toc_entries, tolerance=30):
     merged_entries = []
-    toc_entries.sort(key=lambda x: (x.page, x.level, x.vpos))  # Sort by page, level, and vertical position
+    toc_entries.sort(key=lambda x: (x.pagenum, x.level, -x.start_vpos))  # Sort by page, level, and vertical position
 
     i = 0
     while i < len(toc_entries):
         current_entry = toc_entries[i]
         j = i + 1
-        while j < len(toc_entries) and toc_entries[j].level == current_entry.level and toc_entries[j].page == current_entry.page:
-            if abs(toc_entries[j].vpos - current_entry.vpos) <= tolerance:
+        while j < len(toc_entries) and toc_entries[j].level == current_entry.level and toc_entries[j].pagenum == current_entry.pagenum:
+            if abs(toc_entries[j].start_vpos - current_entry.start_vpos) <= tolerance:
                 # Merge titles
                 current_entry.title += " " + toc_entries[j].title
                 j += 1
@@ -588,7 +595,7 @@ def merge_toc_entries(toc_entries, tolerance=10):
     return merged_entries
 
 @log_time
-def nest_toc_entries(flat_toc):
+def nest_toc_entries(flat_toc: List[ToCEntry]) -> List[ToCEntry]:
     if not flat_toc:
         return []
 
@@ -596,13 +603,17 @@ def nest_toc_entries(flat_toc):
         nested = []
         while entries:
             entry = entries[0]
-            if entry.level > current_level:
+            if entry.level > current_level: # it is a subsection of the current entry
                 if nested:
                     nested[-1].subsections = nest_entries(entries, entry.level)
-            elif entry.level < current_level:
+                    nested[-1].page_range[1] = nested[-1].subsections[-1].page_range[1]
+            elif entry.level < current_level: # it is a parent entry
                 break
-            else:
+            else: # it is a sibling entry, entry_level == current_level
                 nested.append(entries.pop(0))
+                if len(nested) > 1:
+                    nested[-2].page_range[1] = nested[-1].page_range[0]
+                    nested[-2].end_vpos = nested[-1].bbox[3]
         return nested
 
     return nest_entries(flat_toc, flat_toc[0].level)
@@ -627,14 +638,16 @@ def main():
         doc = pickle.load(f)
 
     headers = [
+        {"header": (16, "Comments on Karl Faspers"), "level": 1, "text": [(16, "A \"fitting\" orientation for a positive and illuminating critical review of")]},
         {"header": (54, "Phenomenology and Theology"), "level": 1, "text": [(55, "The popular understanding of the relationship between theology and")]},
         {"header": (58, "THE POSITIVE CHARACTER OF THEOLOGY"), "level": 2, "text": [(58, "A positive science is the founding disclosure of a being that is given")]},
         {"header": (112, "ON THE ESSENCE OF GROUND"), "level": 1, "text": [(113, "but was concerned with understanding their interconnection")]},
-        {"header": (122, "TRANSCENDENCE AS THE DOMAIN OF THE"), "level": 2, "text": [(122, "A preliminary remark on terminology must guide our use of")]}
-    ]
+        {"header": (122, "TRANSCENDENCE AS THE DOMAIN OF THE"), "level": 2, "text": [(122, "A preliminary remark on terminology must guide our use of")]},
+        {"header": (165, "TRUTH AS ERRANCY"), "level": 2, "text": [(165, "As insistent, the human being is turned toward the most readily available")]},
+        {"header": (140, "III. ON THE ESSENCE OF GROUND*"), "level": 2, "text": [(140, "reason or ground to the domain of transcendence")]},    ]
     ign_pattern = re.compile(r'^[a-z]\)')
 
-    tolerances = {"font": 3e-1, "bbox": 1e-1, "text": 5e-1}
+    tolerances = {"font": 3e-1, "bbox": 1e-1, "text": 4e-1}
 
     toc_filter_options: list[dict] = []
     text_filter_options: list[dict] = []
@@ -642,29 +655,33 @@ def main():
     for header in headers:
         toc_filter_option = {
             "toc": ToCFilterOptions(check_bbox=False), 
-            "font": FontFilterOptions(check_colors=False, check_width=False, check_is_upper=header["level"] == 2, size_tolerance=tolerances["font"]),
-            "bbox": None}
+            "font": FontFilterOptions(check_colors=False, check_width=header["level"]==2, check_is_upper=header["level"] == 2, size_tolerance=tolerances["font"], ign_pattern=ign_pattern),
+            "bbox": BoundingBoxFilterOptions()}
         toc_filter_options.append(toc_filter_option)
 
         if args.include_text_filters or True:
             text_filter_option = {
                 "text": TextFilterOptions(check_bbox=False, tolerance=tolerances["text"]),
                 "font": FontFilterOptions(check_colors=False, check_is_upper=False, size_tolerance=tolerances["font"]),
-                "bbox": BoundingBoxFilterOptions(tolerance=tolerances["bbox"])}
+                "bbox": None}
             text_filter_options.append(text_filter_option)
 
     
     recipe = generate_recipe(doc, headers, 
                              page_numbers=None, 
-                             tolerances={"font": 1e-1, "bbox": 1e-1}, 
+                             tolerances={"font": 3e-1, "bbox": 3e-1}, 
                              ign_pattern=ign_pattern, clip=None, 
                              include_text_filters=True,
                              toc_filter_options=toc_filter_options,
                              text_filter_options=text_filter_options)
-    # toc_entries = extract_toc(text_doc, recipe, page_range=page_range)
-    # merged_toc_entries = merge_toc_entries(toc_entries, tolerance=30)
+    
+    page_range = [16,len(doc)-22]
+    toc_entries = recipe.extract_toc(doc, extract_text=True, page_range=page_range)
+    merged_toc_entries = merge_toc_entries(toc_entries, tolerance=30)
+    nested_toc_entries = nest_toc_entries(merged_toc_entries)
 
     print(recipe)
+    print(nested_toc_entries)
 
 if __name__ == "__main__":
     main()
