963fe0b761f577b3f47e405474cdfa6283bdad9a
loganrooks <logansrooks@gmail.com>
2025-01-06 03:46:30 -0500

    add GitHub Actions workflow for publishing; include steps for testing and packaging

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pdf2anki",
-    version="0.1",
+    name="mypdf2anki",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "pdfminer.six",
